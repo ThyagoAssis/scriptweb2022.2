f9ea56cffd96beccb69f0edca2bb80a1cbb4efdf
Fabiano Moreira <professor@fabianomoreira.com.br>
2023-07-11 11:59:48 -0300

    Aula 05

---
+++ Aula 05/src/app/foto/foto.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+//Decorator - Determina a função da classe
+@Component({
+    selector: 'app-foto',
+    templateUrl: './foto.component.html',
+    styleUrls: ['./foto.component.css']
+})
+export class FotoComponent{}
