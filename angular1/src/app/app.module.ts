450b5a7fbf17bb71ba6bcf05eff922d929ad97e9
Fabiano Moreira <professor@fabianomoreira.com.br>
2023-07-18 10:30:36 -0300

    Aula 07

--- angular1/src/app/app.module.ts
+++ angular1/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FotoComponent } from './foto/foto.component';
 import { HeaderComponent } from './header/header.component';
+import { FotolistComponent } from './fotolist/fotolist.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FotoComponent,
-    HeaderComponent
+    HeaderComponent,
+    FotolistComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
